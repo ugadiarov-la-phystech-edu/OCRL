37d928b7dd64a1bc869794f2e1a2171ac08725c0
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2023-08-12 00:21:19 +0300

    Added Navigation environment

--- train_sb3.py
+++ train_sb3.py
@@ -1,10 +1,13 @@
 import logging
 from pathlib import Path
 
+import cv2
+import gym
 import hydra
 import omegaconf
 import stable_baselines3 as sb3
 import wandb
+from gym import spaces
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.utils import set_random_seed
@@ -17,6 +20,33 @@ import sb3s
 from utils.tools import *
 
 log = logging.getLogger(__name__)
+
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env: gym.Env, width: int = 64, height: int = 64):
+        gym.ObservationWrapper.__init__(self, env)
+        self.width = width
+        self.height = height
+        self.observation_space = spaces.Box(
+            low=0, high=255, shape=(self.height, self.width, env.observation_space.shape[2]),
+            dtype=env.observation_space.dtype
+        )
+
+    def observation(self, frame: np.ndarray) -> np.ndarray:
+        frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_CUBIC)
+        return frame
+
+
+class FailOnTimelimitWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def step(self, action):
+        observation, reward, done, info = super().step(action)
+        if done and 'is_success' not in info:
+            info['is_success'] = False
+
+        return observation, reward, done, info
 
 
 @hydra.main(config_path="configs/", config_name="train_sb3")
@@ -40,7 +70,14 @@ def main(config):
         def make_env(seed=0):
             if config.ocr.name == "GT":
                 config.env.render_mode = "state"
-            env = getattr(envs, config.env.env)(config.env, seed)
+            if config.env.name.startswith('Navigation'):
+                env = gym.make(config.env.name)
+                env = WarpFrame(env, width=config.env.obs_size, height=config.env.obs_size)
+                env = FailOnTimelimitWrapper(env)
+                env.seed(seed)
+                env.action_space.seed(seed)
+            else:
+                env = getattr(envs, config.env.env)(config.env, seed)
             env = Monitor(env)  # record stats such as returns
             return env
         env = DummyVecEnv([make_env])
@@ -54,7 +91,14 @@ def main(config):
             def _init():
                 if config.ocr.name == "GT":
                     config.env.render_mode = "state"
-                env = getattr(envs, config.env.env)(config.env, rank + seed)
+                if config.env.name.startswith('Navigation'):
+                    env = gym.make(config.env.name)
+                    env = WarpFrame(env, width=config.env.obs_size, height=config.env.obs_size)
+                    env = FailOnTimelimitWrapper(env)
+                    env.seed(seed + rank)
+                    env.action_space.seed(seed + rank)
+                else:
+                    env = getattr(envs, config.env.env)(config.env, seed + rank)
                 env = Monitor(env)  # record stats such as returns
                 return env
             set_random_seed(seed)
@@ -71,9 +115,16 @@ def main(config):
     # )
     if config.ocr.name == "GT":
         config.env.render_mode = "state"
-    eval_env = getattr(envs, config.env.env)(
-        config.env, seed=config.seed + config.num_envs
-    )
+    if config.env.name.startswith('Navigation'):
+        eval_env = gym.make(config.env.name)
+        eval_env = WarpFrame(eval_env, width=config.env.obs_size, height=config.env.obs_size)
+        eval_env = FailOnTimelimitWrapper(eval_env)
+        eval_env.seed(config.seed + config.num_envs)
+        eval_env.action_space.seed(config.seed + config.num_envs)
+    else:
+        eval_env = getattr(envs, config.env.env)(
+            config.env, seed=config.seed + config.num_envs
+        )
     eval_env = Monitor(eval_env)  # record stats such as returns
     model_kwargs = {
         "verbose": 1,
